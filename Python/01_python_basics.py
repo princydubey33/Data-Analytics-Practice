c11cdc835fca142f944096cffb634c3ed3db3366
Princy Dubey <165326451+princydubey33@users.noreply.github.com>
2026-09-10 17:22:55 +0530

    Add basic Python script for introduction and calculations

---
+++ Python/01_python_basics.py
@@ -0,0 +1,15 @@
+# Python Basics
+name="Princy"
+age=20
+
+print("Hello my name is", name)
+print("I am",age,"years old")
+
+# Basic calculation
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
